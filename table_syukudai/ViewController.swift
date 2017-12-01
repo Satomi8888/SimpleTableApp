8fe844de4bb79e203d4b3757daa6bbcfa476978a
遠山聡美 <S083@inv-238.inv.local>
2017-12-01 12:27:55 +0900

    Initial Commit

---
+++ table_syukudai/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  table_syukudai
+//
+//  Created by 遠山　聡美 on 2017/12/01.
+//  Copyright © 2017年 Simple. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
