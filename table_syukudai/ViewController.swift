a309a34c157c64a27191737ffea288a53c5da7e4
遠山聡美 <S083@inv-238.inv.local>
2017-12-07 13:29:24 +0900

    完成

--- table_syukudai/ViewController.swift
+++ table_syukudai/ViewController.swift
@@ -8,7 +8,23 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    //格納する配列
+    var nameList:[String] = ["康平", "遠山", "亜美"]
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return nameList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+            let nameTitle = nameList[indexPath.row]
+            cell.textLabel?.text = nameTitle
+        
+            return cell
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
